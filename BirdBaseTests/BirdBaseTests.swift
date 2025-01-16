1adebf0ba7d56f26c24d155485d248123000e856
Ton Reurts <ton@reurts.com>
2025-01-16 21:34:11 +0100

    temp

---
+++ BirdBaseTests/BirdBaseTests.swift
@@ -0,0 +1,17 @@
+//
+//  BirdBaseTests.swift
+//  BirdBaseTests
+//
+//  Created by iMac 2019 on 16/01/2025.
+//
+
+import Testing
+@testable import BirdBase
+
+struct BirdBaseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
